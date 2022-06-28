8de6282cd12dd2f0b36a113b374e0baa3db944b1
Adam Hall <adamhalldesigns@gmail.com>
2022-06-28 07:55:10 +0100

    escape on caps lock

--- keyboards/preonic/keymaps/codethread/keymap.c
+++ keyboards/preonic/keymaps/codethread/keymap.c
@@ -17,25 +17,26 @@ enum preonic_keycodes {
   BACKLIT
 };
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_DVORAK] = LAYOUT_preonic_grid(
-    //     ,           ,           ,           ,  F                   ,  G             /* | */  ,  H           ,  J                   ,                      ,           ,           ,  ,
-  KC_ESC   ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6        ,  KC_7                ,  KC_8                ,  KC_9     ,  KC_0     ,  KC_F12,
-  KC_TAB   ,  KC_QUOT  ,  KC_COMM  ,  KC_DOT   ,  KC_P                ,  KC_Y          /* | */  ,  KC_F        ,  KC_G                ,  KC_C                ,  KC_R     ,  KC_L     ,  KC_ENT,
-  KC_LCTL  ,  KC_A     ,  KC_O     ,  KC_E     ,  KC_U                ,  KC_I          /*---*/  ,  KC_D        ,  KC_H                ,  KC_T                ,  KC_N     ,  KC_S     ,  KC_SLSH,
-  KC_LSFT  ,  KC_SCLN  ,  KC_Q     ,  KC_J     ,  KC_K                ,  KC_X          /* | */  ,  KC_B        ,  KC_M                ,  KC_W                ,  KC_V     ,  KC_Z     ,  KC_RSFT,
-  TG(_RAISE)   ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  , ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  QWERTY
+    //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H               ,  J                   ,                 ,           ,           ,  ,
+  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6            ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
+  KC_TAB         ,  KC_QUOT  ,  KC_COMM  ,  KC_DOT   ,  KC_P                ,  KC_Y          /* | */  ,  KC_F            ,  KC_G                ,  KC_C           ,  KC_R     ,  KC_L     ,  KC_ENT,
+  CTL_T(KC_ESC)  ,  KC_A     ,  KC_O     ,  KC_E     ,  KC_U                ,  KC_I          /*---*/  ,  KC_D            ,  KC_H                ,  KC_T           ,  KC_N     ,  KC_S     ,  KC_SLSH,
+  KC_LSFT        ,  KC_SCLN  ,  KC_Q     ,  KC_J     ,  KC_K                ,  KC_X          /* | */  ,  KC_B            ,  KC_M                ,  KC_W           ,  KC_V     ,  KC_Z     ,  KC_RSFT,
+  TG(_RAISE)     ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  QWERTY
 ),
 
 [_QWERTY] = LAYOUT_preonic_grid(
-    //     ,           ,           ,           ,  F                   ,  G             /* | */  ,  H           ,  J                   ,                      ,           ,           ,  ,
-  KC_ESC   ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6        ,  KC_7                ,  KC_8                ,  KC_9     ,  KC_0     ,  KC_F12,
-  KC_TAB   ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T          /* | */  ,  KC_Y        ,  KC_U                ,  KC_I                ,  KC_O     ,  KC_P     ,  KC_ENT,
-  KC_LCTL  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G          /*---*/  ,  KC_H        ,  KC_J                ,  KC_K                ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
-  KC_LSFT  ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B          /* | */  ,  KC_N        ,  KC_M                ,  KC_COMM             ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
-  TG(_RAISE)   ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  DVORAK
+    //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H               ,  J                   ,                 ,           ,           ,  ,
+  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6            ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
+  KC_TAB         ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T          /* | */  ,  KC_Y            ,  KC_U                ,  KC_I           ,  KC_O     ,  KC_P     ,  KC_ENT,
+  CTL_T(KC_ESC)  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G          /*---*/  ,  KC_H            ,  KC_J                ,  KC_K           ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
+  KC_LSFT        ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B          /* | */  ,  KC_N            ,  KC_M                ,  KC_COMM        ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
+  TG(_RAISE)     ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  DVORAK
   ),
 
 [_LOWER] = LAYOUT_preonic_grid(
@@ -57,12 +58,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid(
-    //     ,           ,           ,           ,  F        ,  G        /* | */              ,  H          ,  J                     ,                         ,            ,           ,  ,
-  QK_BOOT  ,  KC_F1    ,  KC_F2    ,  KC_F3    ,  KC_F4    ,  KC_F5    /* | */              ,  KC_F6      ,  KC_F7                 ,  KC_F8                  ,  KC_F9     ,  KC_F10   ,  KC_F11   ,
-  RGB_TOG  ,  _______  ,  KC_MSTP  ,  KC_MPRV  ,  KC_MNXT  ,  KC_MEDIA_PLAY_PAUSE /* | */   ,  _______    ,  G(KC_SPC)             ,  TERM_OFF               ,  _______   ,  _______  ,  _______,
-  RGB_MOD  ,  _______  ,  _______  ,  _______  ,  KC_LALT  ,  KC_LGUI              /*---*/  ,  KC_LEFT    ,  KC_DOWN               ,  KC_DOWN                ,  KC_RIGHT  ,  _______  ,  _______,
-  _______  ,  MUV_DE   ,  MUV_IN   ,  MU_ON    ,  MU_OFF   ,  _______  /* | */              ,  MI_OFF     ,  LSG(KC_LEFT_BRACKET)  ,  LSG(KC_RIGHT_BRACKET)  ,  _______   ,  _______  ,  _______,
-  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  /* | */              ,  _______    ,  _______               ,  _______                ,  _______   ,  _______  ,  _______
+    //     ,           ,           ,           ,  F        ,  G        /* | */              ,  H        ,  J                     ,                         ,            ,           ,  ,
+  QK_BOOT  ,  KC_F1    ,  KC_F2    ,  KC_F3    ,  KC_F4    ,  KC_F5    /* | */              ,  KC_F6    ,  KC_F7                 ,  KC_F8                  ,  KC_F9     ,  KC_F10   ,  KC_F11   ,
+  RGB_TOG  ,  _______  ,  KC_MSTP  ,  KC_MPRV  ,  KC_MNXT  ,  KC_MEDIA_PLAY_PAUSE /* | */   ,  _______  ,  G(KC_SPC)             ,  TERM_OFF               ,  _______   ,  _______  ,  _______,
+  RGB_MOD  ,  _______  ,  _______  ,  _______  ,  KC_LALT  ,  KC_LGUI              /*---*/  ,  KC_LEFT  ,  KC_DOWN               ,  KC_DOWN                ,  KC_RIGHT  ,  _______  ,  _______,
+  _______  ,  MUV_DE   ,  MUV_IN   ,  MU_ON    ,  MU_OFF   ,  _______  /* | */              ,  MI_OFF   ,  LSG(KC_LEFT_BRACKET)  ,  LSG(KC_RIGHT_BRACKET)  ,  _______   ,  _______  ,  _______,
+  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  /* | */              ,  _______  ,  _______               ,  _______                ,  _______   ,  _______  ,  _______
 )
 
 };
