34dd84210c0ab89b8168e0bbca615d05fb80b0e6
Adam Hall <adamhalldesigns@gmail.com>
2022-07-02 12:12:41 +0100

    so long dvorak

--- keyboards/preonic/keymaps/codethread/keymap.c
+++ keyboards/preonic/keymaps/codethread/keymap.c
@@ -2,7 +2,6 @@
 #include "muse.h"
 
 enum preonic_layers {
-  _DVORAK,
   _QWERTY,
   _LOWER,
   _RAISE,
@@ -10,8 +9,7 @@ enum preonic_layers {
 };
 
 enum preonic_keycodes {
-  DVORAK = SAFE_RANGE,
-  QWERTY,
+  QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   BACKLIT
@@ -20,31 +18,21 @@ enum preonic_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-
-[_DVORAK] = LAYOUT_preonic_grid(
-    //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H               ,  J                   ,                 ,           ,           ,  ,
-  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6            ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
-  KC_TAB         ,  KC_QUOT  ,  KC_COMM  ,  KC_DOT   ,  KC_P                ,  KC_Y          /* | */  ,  KC_F            ,  KC_G                ,  KC_C           ,  KC_R     ,  KC_L     ,  KC_ENT,
-  CTL_T(KC_ESC)  ,  KC_A     ,  KC_O     ,  KC_E     ,  KC_U                ,  KC_I          /*---*/  ,  KC_D            ,  KC_H                ,  KC_T           ,  KC_N     ,  KC_S     ,  KC_SLSH,
-  KC_LSFT        ,  KC_SCLN  ,  KC_Q     ,  KC_J     ,  KC_K                ,  KC_X          /* | */  ,  KC_B            ,  KC_M                ,  KC_W           ,  KC_V     ,  KC_Z     ,  KC_RSFT,
-  TG(_RAISE)     ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  QWERTY
-),
-
 [_QWERTY] = LAYOUT_preonic_grid(
     //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H               ,  J                   ,                 ,           ,           ,  ,
   KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6            ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
   KC_TAB         ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T          /* | */  ,  KC_Y            ,  KC_U                ,  KC_I           ,  KC_O     ,  KC_P     ,  KC_ENT,
   CTL_T(KC_ESC)  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G          /*---*/  ,  KC_H            ,  KC_J                ,  KC_K           ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
   KC_LSFT        ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B          /* | */  ,  KC_N            ,  KC_M                ,  KC_COMM        ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
-  TG(_RAISE)     ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  DVORAK
-  ),
+  _______        ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  MO(_RAISE)
+),
 
 [_LOWER] = LAYOUT_preonic_grid(
     //     ,              ,                 ,                 ,  F               ,  G        /* | */  ,  H         ,  J                    ,                        ,             ,                ,  ,
   KC_F14   ,  KC_F1       ,  KC_F2          ,  KC_F3          ,  KC_F4           ,  KC_F5    /* | */  ,  KC_F6     ,  KC_F7                ,  KC_F8                 ,  KC_F9      ,  KC_F10        ,  KC_F11,
   _______  ,  _______     ,  KC_ASTERISK    ,  KC_UNDERSCORE  ,  KC_PERCENT      ,  KC_AT    /* | */  ,  _______   ,  KC_LEFT_BRACKET      ,  KC_RIGHT_BRACKET      ,  KC_DOLLAR  ,  _______       ,  _______,
   _______  ,  KC_EXCLAIM  ,  KC_EQUAL       ,  KC_LEFT_PAREN  ,  KC_RIGHT_PAREN  ,  KC_TILDE /*---*/  ,  KC_MINUS  ,  KC_LEFT_CURLY_BRACE  ,  KC_RIGHT_CURLY_BRACE  ,  KC_GT      ,  KC_BACKSLASH  ,  _______,
-  _______  ,  _______     ,  KC_CIRCUMFLEX  ,  KC_AMPR        ,  KC_PLUS         ,  _______  /* | */  ,  _______   ,  KC_PIPE              ,  KC_GRAVE              ,  A(KC_3)    ,  _______       ,  _______,
+  _______  ,  _______     ,  KC_CIRCUMFLEX  ,  KC_AMPR        ,  KC_PLUS         ,  _______  /* | */  ,  _______   ,  KC_PIPE              ,  KC_GRAVE              ,  KC_HASH    ,  _______       ,  _______,
   _______  ,  _______     ,  _______        ,  _______        ,  MO(_ADJUST)     ,  _______  /* | */  ,  _______   ,  MO(_ADJUST)          ,  _______               ,  _______    ,  _______       ,  _______
 ),
 
@@ -73,12 +61,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case QWERTY:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case DVORAK:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_DVORAK);
           }
           return false;
           break;
