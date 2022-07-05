ae86a9343cdc4f58e1ff128a688bbe0bf1cbb89d
Adam Hall <adamhalldesigns@gmail.com>
2022-07-05 20:26:43 +0100

    easier arrows

--- keyboards/preonic/keymaps/codethread/keymap.c
+++ keyboards/preonic/keymaps/codethread/keymap.c
@@ -19,12 +19,12 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid(
-    //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H               ,  J                   ,                 ,           ,           ,  ,
-  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6            ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
-  KC_TAB         ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T          /* | */  ,  KC_Y            ,  KC_U                ,  KC_I           ,  KC_O     ,  KC_P     ,  KC_ENT,
-  CTL_T(KC_ESC)  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G          /*---*/  ,  KC_H            ,  KC_J                ,  KC_K           ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
-  KC_LSFT        ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B          /* | */  ,  KC_N            ,  KC_M                ,  KC_COMM        ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
-  _______        ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  CTL_T(KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  MO(_RAISE)
+    //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H                    ,  J                   ,                 ,           ,           ,  ,
+  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6                 ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
+  KC_TAB         ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T          /* | */  ,  KC_Y                 ,  KC_U                ,  KC_I           ,  KC_O     ,  KC_P     ,  KC_ENT,
+  CTL_T(KC_ESC)  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G          /*---*/  ,  KC_H                 ,  KC_J                ,  KC_K           ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
+  KC_LSFT        ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B          /* | */  ,  KC_N                 ,  KC_M                ,  KC_COMM        ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
+  _______        ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  LT(_RAISE, KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  MO(_RAISE)
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
