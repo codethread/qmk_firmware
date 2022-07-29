c89276591b67189937f70c9508c3b5c30bd4ff66
Adam Hall <adamhalldesigns@gmail.com>
2022-07-29 16:21:00 +0100

    lots of changes

--- keyboards/preonic/keymaps/codethread/keymap.c
+++ keyboards/preonic/keymaps/codethread/keymap.c
@@ -3,8 +3,9 @@
 
 enum preonic_layers {
   _QWERTY,
+  _SHIFTED,
   _LOWER,
-  _RAISE,
+  _ARROWS,
   _ADJUST
 };
 
@@ -19,12 +20,21 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid(
-    //           ,           ,           ,           ,  F                   ,  G             /* | */  ,  H                    ,  J                   ,                 ,           ,           ,  ,
-  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5          /* | */  ,  KC_6                 ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
-  KC_TAB         ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T          /* | */  ,  KC_Y                 ,  KC_U                ,  KC_I           ,  KC_O     ,  KC_P     ,  KC_ENT,
-  CTL_T(KC_ESC)  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G          /*---*/  ,  KC_H                 ,  KC_J                ,  KC_K           ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
-  KC_LSFT        ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B          /* | */  ,  KC_N                 ,  KC_M                ,  KC_COMM        ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
-  _______        ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  SFT_T(KC_TAB) /* | */  ,  LT(_RAISE, KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  MO(_RAISE)
+    //           ,           ,           ,           ,  F                   ,  G                    /* | */  ,  H                     ,  J                   ,                 ,           ,           ,  ,
+  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4                ,  KC_5                 /* | */  ,  KC_6                  ,  KC_7                ,  KC_8           ,  KC_9     ,  KC_0     ,  KC_F12,
+  KC_TAB         ,  KC_Q     ,  KC_W     ,  KC_E     ,  KC_R                ,  KC_T                 /* | */  ,  KC_Y                  ,  KC_U                ,  KC_I           ,  KC_O     ,  KC_P     ,  KC_ENT,
+  CTL_T(KC_ESC)  ,  KC_A     ,  KC_S     ,  KC_D     ,  KC_F                ,  KC_G                 /*---*/  ,  KC_H                  ,  KC_J                ,  KC_K           ,  KC_L     ,  KC_SCLN  ,  KC_QUOT,
+  KC_LSFT        ,  KC_Z     ,  KC_X     ,  KC_C     ,  KC_V                ,  KC_B                 /* | */  ,  KC_N                  ,  KC_M                ,  KC_COMM        ,  KC_DOT   ,  KC_SLSH  ,  KC_RSFT,
+  MO(_ADJUST)    ,  KC_LCTL  ,  KC_LALT  ,  KC_LGUI  ,  LT(_LOWER, KC_ENT)  ,  LT(_SHIFTED, KC_TAB) /* | */  ,  LT(_ARROWS, KC_BSPC)  ,  LT(_LOWER, KC_SPC)  ,  ALT_T(KC_ESC)  ,  KC_LALT  ,  KC_LCTL  ,  MO(_ARROWS)
+),
+
+[_SHIFTED] = LAYOUT_preonic_grid(
+    //           ,           ,           ,           ,  F        ,  G       /* | */  ,  H        ,  J        ,              ,             ,              ,  ,
+  KC_ESC         ,  KC_1     ,  KC_2     ,  KC_3     ,  KC_4     ,  KC_5    /* | */  ,  KC_6     ,  KC_7     ,  KC_8        ,  KC_9       ,  KC_0        ,  KC_F12,
+  KC_TAB         ,  S(KC_Q)  ,  S(KC_W)  ,  S(KC_E)  ,  S(KC_R)  ,  S(KC_T) /* | */  ,  S(KC_Y)  ,  S(KC_U)  ,  S(KC_I)     ,  S(KC_O)    ,  S(KC_P)     ,  KC_ENT,
+  CTL_T(KC_ESC)  ,  S(KC_A)  ,  S(KC_S)  ,  S(KC_D)  ,  S(KC_F)  ,  S(KC_G) /*---*/  ,  S(KC_H)  ,  S(KC_J)  ,  S(KC_K)     ,  S(KC_L)    ,  S(KC_SCLN)  ,  S(KC_QUOT),
+  KC_LSFT        ,  S(KC_Z)  ,  S(KC_X)  ,  S(KC_C)  ,  S(KC_V)  ,  S(KC_B) /* | */  ,  S(KC_N)  ,  S(KC_M)  ,  S(KC_COMM)  ,  S(KC_DOT)  ,  S(KC_SLSH)  ,  KC_RSFT,
+  _______        ,  _______  ,  _______  ,  _______  ,  _______  ,  _______ /* | */  ,  _______  ,  _______  ,  _______     ,  _______    ,  _______     ,  _______
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
@@ -33,16 +43,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______  ,  _______     ,  KC_ASTERISK    ,  KC_UNDERSCORE  ,  KC_PERCENT      ,  KC_AT    /* | */  ,  _______   ,  KC_LEFT_BRACKET      ,  KC_RIGHT_BRACKET      ,  KC_DOLLAR  ,  _______       ,  _______,
   _______  ,  KC_EXCLAIM  ,  KC_EQUAL       ,  KC_LEFT_PAREN  ,  KC_RIGHT_PAREN  ,  KC_TILDE /*---*/  ,  KC_MINUS  ,  KC_LEFT_CURLY_BRACE  ,  KC_RIGHT_CURLY_BRACE  ,  KC_GT      ,  KC_BACKSLASH  ,  _______,
   _______  ,  _______     ,  KC_CIRCUMFLEX  ,  KC_AMPR        ,  KC_PLUS         ,  _______  /* | */  ,  _______   ,  KC_PIPE              ,  KC_GRAVE              ,  KC_HASH    ,  _______       ,  _______,
-  _______  ,  _______     ,  _______        ,  _______        ,  MO(_ADJUST)     ,  _______  /* | */  ,  _______   ,  MO(_ADJUST)          ,  _______               ,  _______    ,  _______       ,  _______
+  _______  ,  _______     ,  _______        ,  _______        ,  _______         ,  _______  /* | */  ,  _______   ,  _______              ,  _______               ,  _______    ,  _______       ,  _______
 ),
 
-[_RAISE] = LAYOUT_preonic_grid(
-    //     ,           ,           ,           ,  F        ,  G        /* | */  ,  H        ,  J        ,           ,            ,           ,  ,
-  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  /* | */  ,  _______  ,  _______  ,  _______  ,  _______   ,  _______  ,  _______,
-  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  /* | */  ,  _______  ,  _______  ,  _______  ,  _______   ,  _______  ,  _______,
-  _______  ,  _______  ,  KC_NUHS  ,  KC_HASH  ,  _______  ,  _______  /*---*/  ,  KC_LEFT  ,  KC_DOWN  ,  KC_UP    ,  KC_RIGHT  ,  _______  ,  _______,
-  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  /* | */  ,  _______  ,  _______  ,  _______  ,  _______   ,  _______  ,  _______,
-  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  /* | */  ,  _______  ,  _______  ,  _______  ,  _______   ,  _______  ,  _______
+[_ARROWS] = LAYOUT_preonic_grid(
+    //     ,           ,           ,                 ,  F            ,  G        /* | */  ,  H                   ,  J                     ,                         ,                       ,           ,  ,
+  _______  ,  _______  ,  _______  ,  _______        ,  _______      ,  _______  /* | */  ,  _______             ,  _______               ,  _______                ,  _______              ,  _______  ,  _______,
+  _______  ,  _______  ,  _______  ,  G(KC_PLUS)     ,  G(KC_MINUS)  ,  _______  /* | */  ,  G(KC_LEFT_BRACKET)  ,  LSG(KC_LEFT_BRACKET)  ,  LSG(KC_RIGHT_BRACKET)  ,  G(KC_RIGHT_BRACKET)  ,  _______, _______,
+  _______  ,  _______  ,  _______  ,  LSG(KC_GRAVE)  ,  G(KC_GRAVE)  ,  _______  /*---*/  ,  KC_LEFT             ,  KC_DOWN               ,  KC_UP                  ,  KC_RIGHT             ,  _______  ,  _______,
+  _______  ,  _______  ,  _______  ,  _______        ,  _______      ,  _______  /* | */  ,  _______             ,  KC_F14                ,  _______                ,  _______              ,  _______  ,  _______,
+  _______  ,  _______  ,  _______  ,  _______        ,  _______      ,  _______  /* | */  ,  _______             ,  _______               ,  _______                ,  _______              ,  _______  ,  _______
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid(
@@ -67,20 +77,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LOWER:
           if (record->event.pressed) {
             layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            update_tri_layer(_LOWER, _ARROWS, _ADJUST);
           } else {
             layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            update_tri_layer(_LOWER, _ARROWS, _ADJUST);
           }
           return false;
           break;
         case RAISE:
           if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            layer_on(_ARROWS);
+            update_tri_layer(_LOWER, _ARROWS, _ADJUST);
           } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            layer_off(_ARROWS);
+            update_tri_layer(_LOWER, _ARROWS, _ADJUST);
           }
           return false;
           break;
@@ -116,7 +126,7 @@ uint16_t muse_tempo = 50;
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
   if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
+    if (IS_LAYER_ON(_ARROWS)) {
       if (clockwise) {
         muse_offset++;
       } else {
