e67d65b5785439566410e4bb6a0b7ea86ab12818
adam.hall <adam.hall@perkbox.com>
2023-06-30 12:39:48 +0100

    fix keycodes

--- keyboards/preonic/keymaps/codethread/config.h
+++ keyboards/preonic/keymaps/codethread/config.h
@@ -1,3 +1,8 @@
 #pragma once
 
-#define TAPPING_TERM 160 // default 200
+// If you find that you frequently trigger the modifier of your mod-tap(s) by accident,
+// for example, that's a sign that your tapping term may be too low so tap DT_UP a few
+// times to increase the tapping term until that no longer happens. On the flip side,
+// if you get superfluous characters when you actually intended to momentarily activate
+// a layer, tap DT_DOWN to lower the tapping term.
+#define TAPPING_TERM 100 // default 200
